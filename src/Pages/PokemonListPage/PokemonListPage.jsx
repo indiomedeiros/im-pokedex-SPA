3347e03fdfa9be35f1e74fc4720b3beac6014d3e
Índio Medeiros <71137294+indiomedeiros@users.noreply.github.com>
2023-12-21 16:27:45 -0300

    [refactor] limita a quantidade de cartões e faz alinhamento.

--- src/Pages/PokemonListPage/PokemonListPage.jsx
+++ src/Pages/PokemonListPage/PokemonListPage.jsx
@@ -1,22 +1,26 @@
 import { useContext } from "react";
 import GlobalContext from "../../global/GlobalContext";
-import { DivHome, DivPokeList, TitlePage } from "./PokemonListPageStyles";
+import {
+  DivHome,
+  DivPokeList,
+  MainContainer,
+  TitlePage,
+} from "./PokemonListPageStyles";
 import { mapPokemons, sortPokemons } from "../../utils/pokemonUtils";
 
 const PokemonListPage = () => {
   const { getters } = useContext(GlobalContext);
 
-    const pokemonsSorted = sortPokemons(getters.pokemons)
-    
-    const pokemonsMapped = mapPokemons(pokemonsSorted) 
-  
+  const pokemonsSorted = sortPokemons(getters.pokemons);
+
+  const pokemonsMapped = mapPokemons(pokemonsSorted);
+
   return (
     <DivHome>
-      <TitlePage>Todos Pokémons</TitlePage>
-
-      <DivPokeList>
-        {pokemonsMapped}
-      </DivPokeList>
+      <MainContainer>
+        <TitlePage>Todos Pokémons</TitlePage>
+        <DivPokeList>{pokemonsMapped}</DivPokeList>
+      </MainContainer>
     </DivHome>
   );
 };
--- src/Pages/PokemonListPage/PokemonListPageStyles.jsx
+++ src/Pages/PokemonListPage/PokemonListPageStyles.jsx
@@ -1,20 +1,28 @@
 import styled from "styled-components";
 
+export const DivHome = styled.div`
+  background: #737373;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
+export const MainContainer = styled.main`
+  display: grid;
+  grid-template-rows: 0.15fr 1fr;
+  max-width: 1440px;
+`;
+export const TitlePage = styled.div`
+  color: white;
+  padding: 60px 0px 25px 30px;
+  text-align: left;
+  font-size: 3rem;
+  font-family: sans-serif;
+  font-weight: 700;
+`;
+
 export const DivPokeList = styled.div`
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
-  height: 100%;
-`;
-
-export const TitlePage = styled.div`
-  color: white;
-  padding: 60px 0px 25px 70px;
-  text-align: left;
-  font-size: 3rem;
-  font-family: sans-serif;
-`;
-export const DivHome = styled.div`
-  background: #737373;
-  min-height: 80vh;
 `;
